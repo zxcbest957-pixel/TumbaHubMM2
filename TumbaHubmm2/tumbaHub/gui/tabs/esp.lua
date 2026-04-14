d49281a99fac849beb1d45191a1f6bfa51d63949
zxcbest957-pixel <zxcbest957@gmail.com>
2026-04-14 23:38:35 +0300

    Add files via upload

--- TumbaHubmm2/tumbaHub/gui/tabs/esp.lua
+++ TumbaHubmm2/tumbaHub/gui/tabs/esp.lua
@@ -23,7 +23,9 @@ Mega.Objects.TabFrames[tabKey] = TabFrame
 -- Main ESP Section
 local MainSection = UI.CreateSection(TabFrame, "section_esp_main")
 
-UI.CreateToggleWithSettings(MainSection, "toggle_esp", "ESP.Enabled", nil, {
+UI.CreateToggleWithSettings(MainSection, "toggle_esp", "ESP.Enabled", function(v)
+    if Mega.Features.ESP then Mega.Features.ESP.SetEnabled(v) end
+end, {
     UI.CreateToggle(nil, "toggle_esp_boxes", "ESP.Boxes"),
     UI.CreateToggle(nil, "toggle_esp_names", "ESP.Names"),
     UI.CreateToggle(nil, "toggle_esp_role", "ESP.ShowRole"),
@@ -31,5 +33,15 @@ UI.CreateToggleWithSettings(MainSection, "toggle_esp", "ESP.Enabled", nil, {
     UI.CreateToggle(nil, "toggle_esp_tracers", "ESP.Tracers"),
     UI.CreateSlider(nil, "slider_esp_max_dist", "ESP.MaxDistance", 50, 5000)
 })
+
+-- Detailed ESP Section
+local DetailSection = UI.CreateSection(TabFrame, "section_esp_details")
+
+UI.CreateToggle(DetailSection, "toggle_esp_skeleton", "ESP.Skeleton")
+UI.CreateToggle(DetailSection, "toggle_esp_health", "ESP.HealthBar")
+UI.CreateToggle(DetailSection, "toggle_esp_items", "ESP.ShowItems")
+UI.CreateToggle(DetailSection, "toggle_esp_dead", "ESP.ShowDead")
+UI.CreateToggle(DetailSection, "toggle_esp_icons", "ESP.UseIcons")
+UI.CreateToggle(DetailSection, "toggle_esp_gundrop", "ESP.GunDrop")
 
 return TabFrame
